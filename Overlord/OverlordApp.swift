c3a32be2a5099ae1c7f5c14bc6ab073dec2aad5d
welshofer <welshofer@gmail.com>
2024-03-22 19:04:38 -0700

    Initial Commit

---
+++ Overlord/OverlordApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlordApp.swift
+//  Overlord
+//
+//  Created by Jay Welshofer on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct OverlordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
